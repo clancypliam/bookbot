1e1c92d027bca9ac1f628080c9a09d942b36e4a4
clancypliam <clancy.p.liam@gmail.com>
2025-10-26 22:02:18 -0500

    formatting in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_char
+from stats import count_words, count_char, sort_scribe
 
 def get_book_text(filepath): 
     with open(filepath) as file:
@@ -7,7 +7,15 @@ def get_book_text(filepath):
 def main():
     relative_path = "books/frankenstein.txt"
     book = get_book_text(relative_path)
-    print(count_words(book), count_char(book))
+    print("============BOOKBOT============")
+    print("Analyzing book found at books/frankenstein.txt")
+    print("---------- Word Count ---------")
+    print(count_words(book))
+    print("------- Character Count -------")
+    for item in sort_scribe(count_char(book)):
+        char = item["char"]
+        num = item["num"]
+        print(f"{char}: {num}")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,5 +1,8 @@
 
 
+def sort_on(items):
+    return items["num"]
+
 def count_words(text):
     count = len(text.split())
     return f"Found {count} total words."
@@ -13,3 +16,12 @@ def count_char(text):
         else:
             scribe[char] += 1
     return scribe
+
+def sort_scribe(dictionary):
+    list =[]
+    for char, num in dictionary.items():
+        if char.isalpha():
+            scribe = {"char": char, "num": num}
+            list.append(scribe)
+    list.sort(reverse=True, key=sort_on)
+    return list
